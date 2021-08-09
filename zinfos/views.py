5469ade18f14cc457fdf8843f1937c3c65cd4e5c
Laurent Lavaud <fidelio33b@gmail.com>
2021-08-09 07:59:51 +0200

    reformat code

--- zinfos/views.py
+++ zinfos/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
 from common.ortc import ORTC
-from common.config import params
 from common.utils import get_orthanc_server
 
-# La page d'accueil                                                                                 
-def index(request):
 
+# La page d'accueil
+def index(request):
     stats = None
-    
+
     # Récupère les statistiques
     orthanc_server = get_orthanc_server()
-    o = ORTC(orthanc_server['host'],
-             orthanc_server['port'],
-             orthanc_server['user'],
-             orthanc_server['password'],
+    o = ORTC(
+        orthanc_server['host'],
+        orthanc_server['port'],
+        orthanc_server['user'],
+        orthanc_server['password'],
     )
     stats = o.GetStatistics()
 
